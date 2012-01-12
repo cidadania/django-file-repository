c885be720399c83a921162a4efba6c3c98e95337
Oscar Carballal Prego <info@oscarcp.com>
2012-01-12 18:04:27 +0100

    Finished installing django-registration and created first templates

--- src/cidadania_docrepo/settings.py
+++ src/cidadania_docrepo/settings.py
@@ -134,6 +134,8 @@ INSTALLED_APPS = (
     'cidadania_docrepo.apps.repository',
 )
 
+ACCOUNT_ACTIVATION_DAYS = 7
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
