7be0a76ca2a4bccbc9db05080beb1e69f11da783
Oscar Carballal Prego <info@oscarcp.com>
2012-01-17 18:00:15 +0100

    Added static and media files urls

--- src/djangofr/urls.py
+++ src/djangofr/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -12,5 +14,16 @@ urlpatterns = patterns('',
     url(r'^user/', include('registration.backends.default.urls')),
 
     url(r'^i18n/', include('django.conf.urls.i18n')),
+
+    # Static content #### FOR DEVELOPMENT!! ####
+    (r'^uploads/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': 'uploads'}),
     
+    # Static content #### FOR DEVELOPMENT!! ####
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': 'static'}),
 )
+
+if settings.DEBUG:
+    # Serve static files
+    urlpatterns += staticfiles_urlpatterns()
