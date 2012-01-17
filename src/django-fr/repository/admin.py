5ad666f0a5015f8a2022247f60b9188b00047b52
Oscar Carballal Prego <info@oscarcp.com>
2012-01-17 13:54:04 +0100

    Added administration interface for repository files and categories

---
+++ src/django-fr/repository/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from django-fr.repository.models import Categories, RepoFile
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    """
+    """
+    list_display = ('name', 'pub_date', 'last_mod')
+    search_fields = ('name',)
+    
+class RepoFileAdmin(admin.ModelAdmin):
+
+    """
+    """
+    list_display = ('name', 'category', 'description', 'public', 'pub_date')
+    search_fields = ('name', 'category', 'description')
+    
+    fieldsets = [
+        (None, {'fields':
+            [('name', 'category'), 'description']}),
+            
+        (None, {'fields':
+            ['public',]})
+    ]
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(RepoFile, RepoFileAdmin)
