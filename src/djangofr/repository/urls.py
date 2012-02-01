2f2f4a3f424dab1095f815b1e96b548e2468ab77
Oscar Carballal Prego <info@oscarcp.com>
2012-02-01 09:59:54 +0100

    ADded ListCategories url

--- src/djangofr/repository/urls.py
+++ src/djangofr/repository/urls.py
@@ -21,13 +21,15 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 
-from djangofr.repository.views import DeleteFile, ViewFile
+from djangofr.repository.views import DeleteFile, ViewFile, ListCategories
 
 urlpatterns = patterns('djangofr.repository.views',
 
     url(r'^add/$', 'add_file', name='add_file'),
     
     url(r'^cat/(?P<cat_id>\d+)/', 'cat_show', name='category_view'),
+
+    url(r'^cat/', ListCategories.as_view(), name='category_list'),
 
     url(r'^myfiles/', 'user_files', name='my_files'),
     
